74c085d71776c2319e7ea2c4ac50c1435fa36f73
Emmanuel Wambugu Ndiritu <emmanuelwambugu5@gmail.com>
2025-04-23 14:22:32 +0300

    Made the styles folder and updated file paths for the css files moved into it.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './Styles/index.css'
+import App from './Styles/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
