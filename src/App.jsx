e95165e0f175408de7f833a3bb3bacd1256eb926
+ <masaitaboi@gmail.com>
2025-04-24 14:50:59 +0300

    one

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './Styles/App.css'
+import POS from './Components/POS/POS'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='App'>
+      <POS/>
       
-    </>
+    </div>
   )
 }
 
